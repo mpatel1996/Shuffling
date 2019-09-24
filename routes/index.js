3d90206cf86f9bced68ee62a22e5ee26217e3f06
John Escalona <johnrichmondescalona@gmail.com>
2019-09-24 10:35:20 -0700

    Restart

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-//temporary HTTP service
-router.get("/john-a4", function(req, res) {
-  res.send("This is John Escalona. Assingment 4 Done.");
-});
-
-module.exports = router;
