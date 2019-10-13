50aa59117b2178e00a70634334f67a793c0b71a2
John Escalona <johnrichmondescalona@gmail.com>
2019-10-13 11:10:38 -0700

    Added userDeck.ejs, dummy data, mtgsdk for testing

--- app.js
+++ app.js
@@ -1,46 +1,40 @@
-var express = require("express"),
+//-----------------------------------------//
+//                REQUIRES                 //
+//-----------------------------------------//
+const express = require("express"),
   mongoose = require("mongoose"),
   User = require("./models/user"),
+  Card = require("./models/card"),
   bodyParser = require("body-parser"),
-  cities = require("cities"); // lib to lookup cities by zip or lat-long
-  colors = require("colors"); //library to use colors for text
-app = express();
+  cities = require("cities"), // lib to lookup cities by zip or lat-long
+  mtg = require("mtgsdk"), //magic the gathering sdk
+  app = express();
 
 //set .ejs as extension for views
 app.set("view engine", "ejs");
+
 //use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// For Twitter Bot
-const Twitter = require('twitter');
-const config = require('./config.js');
-const T = new Twitter(config);
-
-const params = {
-  q: '#software',
-  count: 10,
-  result_type: 'recent',
-  lang: 'en'
-}
-// End use of twitter bot stuff
-
-//PORT
-const PORT = 3000;
-
-//connect to mongoDB Atlas
+//-----------------------------------------//
+//        MongoDB Atlas CONNECTION         //
+//-----------------------------------------//
 const connectionString =
   "mongodb+srv://jrescalona:Testing2019@cluster0-e1ig7.mongodb.net/users?retryWrites=true&w=majority";
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//TODO: create new db for cards only.
 
-//index route
+//-----------------------------------------//
+//                ROUTES                   //
+//-----------------------------------------//
+//index routes
 app.get("/", function(req, res) {
   res.render("index");
 });
 
-//default route
-app.get("*", function(req, res) {
-  res.render("/index");
+app.get("/index", function(req, res) {
+  res.render("index");
 });
 
 //handle new user requests
@@ -51,60 +45,60 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   //request location from Cities library
   let userAddress = cities.zip_lookup(req.body.zipCode);
-  res.send(
-    "<h2>Welcome " +
-      req.body.firstName +
-      " " +
-      req.body.lastName +
-      "! </h2><h3>from " +
-      userAddress.city +
-      ", " +
-      userAddress.state_abbr +
-      "</h3>"
-  );
-  
-  //TODO: database integration
-  let city, state, zipCode, country;
-   colors.setTheme({
-    error: red,
-    warning: yellow,
-   }
-});
-
-
-// Added twitter bot that prints tweets link for #software to console.
-T.get('search/tweets', params, (err, data, response) => {
-  // If there is no error, proceed
-  if(err){
-    return console.log(err);
-  }
-
-  // Loop through the returned tweets
-  const tweetsId = data.statuses
-    .map(tweet => ({ id: tweet.id_str }));
-
-  tweetsId.map(tweetId => {
-    T.post('favorites/create', tweetId, (err, response) => {
-      if(err){
-        return console.log(err[0].message);
+  User.create(
+    {
+      userFirstName: req.body.firstName,
+      userLastName: req.body.lastName,
+      city: userAddress.city,
+      state: userAddress.state,
+      zipCode: userAddress.zipcode
+    },
+    function(err, newUser) {
+      if (err) {
+        console.log(err[0].message);
+      } else {
+        console.log(newUser);
+        res.render("index");
       }
-
-      const username = response.user.screen_name;
-      const favoritedTweetId = response.id_str;
-	  console.log(`Favorited: https://twitter.com/${username}/status/${favoritedTweetId}`);
-
-    });
-  });
-})
-
-// Ended
-
-//default route
-app.get("*", function(req, res) {
-  res.render("/index");
+    }
+  );
 });
 
-//listen
+//-----------------------------------------//
+//        POPULATE DB (TEST ONLY)          //
+//-----------------------------------------//
+app.post("/getCards", (req, res) => {
+  mtg.card.all({ name: req.body.categoryName, pageSize: 1 }).on("data", card => {
+    Card.create(
+      {
+        name: card.name,
+        imageUrl: card.imageUrl
+      },
+      function(err, cards) {
+        if (err) {
+          console.log("Error in cards retrieval:" + err);
+        } else {
+          console.log(cards);
+          res.render("index");
+        }
+      }
+    );
+  });
+});
+
+//userCards route
+var fs = require("fs");
+
+app.get("/userDeck", function(req, res) {
+  var data = fs.readFileSync("./test/data.json");
+  var cards = JSON.parse(data);
+  res.render("userDeck", { cards: cards });
+});
+
+//-----------------------------------------//
+//                LISTEN                   //
+//-----------------------------------------//
+const PORT = 3000;
 app.listen(PORT, function(res, req) {
   console.log("Server Started on " + PORT);
 });
