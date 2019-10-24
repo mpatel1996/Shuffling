51e005c06b76cbb4e611d8571e48a885b3ba1830
bhitt <BrandenHitt@gmail.com>
2019-10-24 16:12:18 -0700

    +v2
    Signed-off-by: bhitt <BrandenHitt@gmail.com>

---
+++ Goorm versions/The_Shuffling_V2/app.js
@@ -0,0 +1,77 @@
+//================================================//
+//         npm Packages                          //
+//==============================================//
+//ide-run.goorm.io/workspace/The_Shuffling?language=us&theme=-dark#
+//keep the order
+
+var mongoose              = require("mongoose"),
+	flash				  = require("connect-flash"),
+	passport              = require("passport"),
+	bodyParser            = require("body-parser"),
+	User                  = require("./models/user"),
+	LocalStrategy         = require("passport-local"),
+	passportLocalMongoose = require("passport-local-mongoose"),
+	express               = require("express"),
+	app                   = express();
+
+//require routes
+var indexRoutes       = require("./routes/index");
+var userDecksRoutes = require("./routes/userDecks");
+
+//configure mongoose
+mongoose.connect("mongodb://localhost/the_shuffling", { useNewUrlParser: true, useUnifiedTopology:true });
+//mongoose.connect("mongodb+srv://bhitt:cs4800mongodb@theshuffling-rk1we.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology:true });
+
+//configre view engine
+app.set("view engine","ejs");
+
+//pathing to correct public file
+app.use(express.static(__dirname + "/public"));
+
+//authentication stuff
+app.use(require("express-session")({
+	//statement used to encode the session
+	secret: "You are the sun, we are the moon",
+	resave: false,
+	saveUninitialized: false
+}));
+
+//set up passport to work in application
+app.use(passport.initialize());
+app.use(passport.session());
+
+//read the session, take data and dencode it..... then encode it and put it back in the session 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//serve public files
+app.use(express.static("public"));
+
+//parses info from the body of a page
+app.use(bodyParser.urlencoded({extended: true}));
+
+//initialize flash for flash messages
+app.use(flash());
+
+//middleware used in every route
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
+//import routes
+app.use("/", indexRoutes);
+app.use("/userDecks", userDecksRoutes);
+
+//Default Route   - sends to index page, keep this route last
+app.get("/*", function(req, res){
+	res.redirect("/");
+});
+
+//listen or start server
+app.listen(3000, function(){
+	console.log("Server is listening on port 3000.....");
+});
---
+++ Goorm versions/The_Shuffling_V2/middleware/index.js
@@ -0,0 +1,14 @@
+//MIDDLEWARE
+var middlewareObj = {};
+
+
+//Middleware
+middlewareObj.isLoggedIn = function(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    req.flash("error", "You need to be logged in to do that");
+    res.redirect("/login");
+}
+
+module.exports = middlewareObj;
