22fdacf727dc5131f8c40e7606a6ed2c14871c80
John Escalona <johnrichmondescalona@gmail.com>
2019-11-05 21:23:45 -0800

    Added Decks section to editCollection & implemented remove from addedCards

--- The_Shuffling_V2/routes/dashboard.js
+++ The_Shuffling_V2/routes/dashboard.js
@@ -7,24 +7,17 @@ const express = require("express"),
   mongoose = require("mongoose"),
   db_keys = require("../config/db_keys");
 
-const userName = "Fuh";
-global.addedCards = [
-  {
-    id: "7779910e-0785-5301-bc7d-f01e01cb85ae",
-    name: "Squeeze",
-    convertedManaCost: 4,
-    rarity: "Rare",
-    imageUrl: "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=19685&type=card",
-    text: "Sorcery spells cost {3} more to cast."
-  }
-];
-global.searchResults = [];
+var addedCards = [], 
+  searchResults = [], 
+  currentCollection = [],
+  groups = []; //contains card groups
+
 Card.find({}, (err, cards) => {
   if (err) {
     console.log(err);
     res.status(500).send();
   } else {
-    global.currentCollection = cards.slice();
+    currentCollection = cards.slice();
   }
 });
 
@@ -32,6 +25,7 @@ Card.find({}, (err, cards) => {
 var fs = require("fs");
 var data = fs.readFileSync("./temp/data.json");
 var decks = JSON.parse(data);
+
 // CONNECT TO MONGODB ATLAS //
 mongoose.connect(db_keys.mongo_uri_users, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -41,7 +35,28 @@ router
     //render available groupings
     searchResults = []; //initialize searchResults
     addedCards = []; //initialize addedCards
+    // get decks
     res.render("dashboard");
+  })
+  .get("/editCollection", (req, res, next) => {    
+
+    //GROUP TEST DATA SET//
+    groups = [];
+    let start=0;
+    for(let i = 0; i < 10; i++) {
+      groups.push({deckName:"Group# " + i, cards:[]})
+      for(let j = start; j < start+60; j++) {
+        groups[i].cards.push(currentCollection[j]);
+      }
+      start += 60;
+    } //GROUP TEST DATA SET//
+    
+    res.render("editCollection", {
+      groups:groups,
+      currentCollection: currentCollection,
+      searchResults: searchResults,
+      addedCards: addedCards
+    })
   })
   .get("/allCards", (req, res) => {
     Card.find((err, cards) => {
@@ -53,14 +68,6 @@ router
       }
     });
   })
-  .get("/editCollection", (req, res, next) => {
-    console.log(addedCards);
-    res.render("editCollection", {
-      currentCollection: currentCollection,
-      searchResults: searchResults,
-      addedCards: addedCards
-    });
-  })
   .get("/userDecks", function(req, res) {
     res.render("userDecks", { decks: decks });
   })
@@ -70,6 +77,11 @@ router
 
 // POST REQUESTS //
 router
+  .post("/editCollection/addGroup", (req,res,next)=> {
+    console.log("Ready to add groups");
+    res.redirect("/editCollection")
+  })
+
   .post("/editCollection/searchCards", (req, res, next) => {
     searchCards(req.body.searchKey, res);
   })
@@ -82,23 +94,21 @@ router
     res.send({ card: card }); //TODO: refactor? no need to send back. access card object from client
   })
   .post("/editCollection/removeCards", (req, res, next) => {
-    //removeCard(req.body.id);
-    let id = res.body.id;
+    let id = req.body.id;
     addedCards = addedCards.filter(card => {
       return id.localeCompare(card.id) !== 0;
-    })[0];
-    console.log("Update Added Cards = " + addedCards);
+    });
+    res.redirect("/editCollection")
   })
   .post("/testCollection", (req, res, next) => {
     let query = req.body.searchkey;
     mtg.card.where({ supertypes: query }).then(cards => {});
   });
 
-
   // TEMPORARY ROUTE(S) //
   router.post("/populate", (req, res)=> {
-    mtg.card.where({ name: req.body.searchKey }).then(cards => {
-      cards.map((card)=> {
+    mtg.card.where({ name: req.body.searchKey }).then(birds => {
+      birds.map((card)=> {
         Card.create(
           {
             id: card.id,
@@ -115,11 +125,6 @@ router
   });
   
 // FUNCTIONS : NEEDS REFACTORING //
-
-async function removeCard(id) {
-  console.log("Remove: " + id);
-}
-
 async function searchCards(key, res) {
   let promise = mtg.card.where({ name: key }).then(cards => {
     return cards;
